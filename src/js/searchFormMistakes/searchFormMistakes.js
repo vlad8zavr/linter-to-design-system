2b0d3ca6a3d73098da3e21a9f92f52fadba11665
vlad8zavr <vlad8zavr@gmail.com>
2019-07-23 22:41:47 +0300

    only mistake 5

--- src/js/searchFormMistakes/searchFormMistakes.js
+++ src/js/searchFormMistakes/searchFormMistakes.js
@@ -303,8 +303,8 @@ export default function(positionOfForms) {
             //checkMistake1(position);
             //checkMistake2(position);
             //checkMistake3(position);
-            checkMistake4(position);
-            // checkMistake5(position);
+            //checkMistake4(position);
+            checkMistake5(position);
             // checkMistake6(position);
             // checkMistake7(position);
             // checkMistake8(position);
